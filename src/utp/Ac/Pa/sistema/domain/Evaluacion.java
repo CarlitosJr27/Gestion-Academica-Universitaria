e6fbacb18ff45ff20d41a33084ea2f826c68d6b5
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:25:40 -0500

    Evaluacion.java

--- src/utp/Ac/Pa/sistema/domain/Evaluacion.java
+++ src/utp/Ac/Pa/sistema/domain/Evaluacion.java
@@ -1,1 +1,22 @@
+package utp.Ac.Pa.sistema.domain;
+
+public class Evaluacion {
+    private String tipo;
+    private double nota;
+    private String fecha;
+    private Estudiante estudiante;
+    private Asignatura asignatura;
+
+    public Evaluacion(String tipo, double nota, String fecha, Estudiante estudiante, Asignatura asignatura) {
+        this.tipo = tipo;
+        this.nota = nota;
+        this.fecha = fecha;
+        this.estudiante = estudiante;
+        this.asignatura = asignatura;
+    }
+
+    public double calcularPromedio() {
+        return nota; // simplificado
+    }
+}
 
