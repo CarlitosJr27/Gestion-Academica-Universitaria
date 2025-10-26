c65f1b14e658ca1becc56c58926e465af482eb91
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:22:26 -0500

    Usuario.java

--- src/utp/Ac/Pa/sistema/domain/Usuario.java
+++ src/utp/Ac/Pa/sistema/domain/Usuario.java
@@ -1,1 +1,22 @@
 
+package utp.Ac.Pa.sistema.domain;
+
+public class Usuario {
+    private String username;
+    private String password;
+    private Rol rol;
+
+    public Usuario(String username, String password, Rol rol) {
+        this.username = username;
+        this.password = password;
+        this.rol = rol;
+    }
+
+    public boolean autenticar(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
+
+    public void cambiarPassword(String nuevaPassword) {
+        this.password = nuevaPassword;
+    }
+}
