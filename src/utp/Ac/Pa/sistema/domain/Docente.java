2ab24ac239b04b9284258a162e6a7fcc15cb59bd
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:21:43 -0500

    Docente.java

--- src/utp/Ac/Pa/sistema/domain/Docente.java
+++ src/utp/Ac/Pa/sistema/domain/Docente.java
@@ -1,1 +1,26 @@
+package utp.Ac.Pa.sistema.domain;
+
+import java.util.List;
+
+public class Docente {
+    private String id;
+    private String nombre;
+    private String correo;
+    private List<String> asignaturas;
+
+    public Docente(String id, String nombre, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public void asignarAsignatura(String asignatura) {
+        this.asignaturas.add(asignatura);
+    }
+
+    public void editar(String nuevoNombre, String nuevoCorreo) {
+        this.nombre = nuevoNombre;
+        this.correo = nuevoCorreo;
+    }
+}
 
