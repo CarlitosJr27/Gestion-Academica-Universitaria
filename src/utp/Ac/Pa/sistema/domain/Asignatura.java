e25ea55da858e6d125e4fa1e205f8e29647aa34e
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:23:28 -0500

    Asignatura.java

--- src/utp/Ac/Pa/sistema/domain/Asignatura.java
+++ src/utp/Ac/Pa/sistema/domain/Asignatura.java
@@ -1,1 +1,21 @@
+package utp.Ac.Pa.sistema.domain;
 
+public class Asignatura {
+    private String codigo;
+    private String nombre;
+    private int creditos;
+
+    public Asignatura(String codigo, String nombre, int creditos) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+    }
+
+    public void asignarGrupo(String grupoId) {
+        // lógica de asignación
+    }
+
+    public void obtenerEvaluaciones() {
+        // lógica de consulta
+    }
+}
