78c3be6e1caf95883208f170b790c06e041231e4
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:28:02 -0500

    Requisito.java

--- src/utp/Ac/Pa/sistema/domain/Requisito.java
+++ src/utp/Ac/Pa/sistema/domain/Requisito.java
@@ -1,1 +1,17 @@
+package utp.Ac.Pa.sistema.domain;
+
+public class Requisito {
+    private String descripcion;
+    private Asignatura asignatura;
+
+    public Requisito(String descripcion, Asignatura asignatura) {
+        this.descripcion = descripcion;
+        this.asignatura = asignatura;
+    }
+
+    public boolean verificarCumplimiento(Estudiante estudiante) {
+        // lógica de verificación
+        return true;
+    }
+}
 
