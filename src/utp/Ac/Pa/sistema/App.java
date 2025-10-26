647c4ca788f2c1b5efe47a91ab539f0b5775cbd1
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:06:47 -0500

    App.java

--- src/utp/Ac/Pa/sistema/App.java
+++ src/utp/Ac/Pa/sistema/App.java
@@ -1,5 +1,7 @@
+package utp.Ac.Pa.sistema;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Sistema Académico Iniciado");
+        System.out.println("Sistema de Gestión Académica Universitaria iniciado.");
     }
 }
