257e374e79f27850a57c7349987709504ecbd3f7
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:26:08 -0500

    Reporte.java

--- src/utp/Ac/Pa/sistema/domain/Reporte.java
+++ src/utp/Ac/Pa/sistema/domain/Reporte.java
@@ -1,1 +1,22 @@
+package utp.Ac.Pa.sistema.domain;
+
+public class Reporte {
+    private Estudiante estudiante;
+    private double promedio;
+    private String observaciones;
+
+    public Reporte(Estudiante estudiante, double promedio, String observaciones) {
+        this.estudiante = estudiante;
+        this.promedio = promedio;
+        this.observaciones = observaciones;
+    }
+
+    public void generar() {
+        // lógica de generación
+    }
+
+    public void exportarPDF() {
+        // lógica de exportación
+    }
+}
 
