cd421a1cae2699d624877cb6ce77a2a89525ee84
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:25:16 -0500

    Horario.java

--- src/utp/Ac/Pa/sistema/domain/Horario.java
+++ src/utp/Ac/Pa/sistema/domain/Horario.java
@@ -1,1 +1,19 @@
+package utp.Ac.Pa.sistema.domain;
+
+public class Horario {
+    private String dia;
+    private String horaInicio;
+    private String horaFin;
+
+    public Horario(String dia, String horaInicio, String horaFin) {
+        this.dia = dia;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+    }
+
+    public boolean validarConflicto(Horario otro) {
+        return this.dia.equals(otro.dia) &&
+               this.horaInicio.equals(otro.horaInicio);
+    }
+}
 
