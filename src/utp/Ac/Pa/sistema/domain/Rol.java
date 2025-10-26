27fb37bd073d09af9bce2383a1df1bbe69e9035a
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:22:50 -0500

    Rol.java

--- src/utp/Ac/Pa/sistema/domain/Rol.java
+++ src/utp/Ac/Pa/sistema/domain/Rol.java
@@ -1,1 +1,21 @@
+package utp.Ac.Pa.sistema.domain;
 
+import java.util.List;
+
+public class Rol {
+    private String nombre;
+    private List<String> permisos;
+
+    public Rol(String nombre, List<String> permisos) {
+        this.nombre = nombre;
+        this.permisos = permisos;
+    }
+
+    public void asignarPermiso(String permiso) {
+        this.permisos.add(permiso);
+    }
+
+    public boolean validarAcceso(String permiso) {
+        return permisos.contains(permiso);
+    }
+}
