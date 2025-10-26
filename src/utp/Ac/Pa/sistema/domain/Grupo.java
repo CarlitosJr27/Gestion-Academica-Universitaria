eb374dabb16d9bdf8b0d55ade7ed87fc516a1a9d
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:24:49 -0500

    Grupo.java

--- src/utp/Ac/Pa/sistema/domain/Grupo.java
+++ src/utp/Ac/Pa/sistema/domain/Grupo.java
@@ -1,1 +1,24 @@
+package utp.Ac.Pa.sistema.domain;
 
+import java.util.List;
+
+public class Grupo {
+    private String id;
+    private List<Estudiante> estudiantes;
+    private Docente docente;
+    private Asignatura asignatura;
+
+    public Grupo(String id, Docente docente, Asignatura asignatura) {
+        this.id = id;
+        this.docente = docente;
+        this.asignatura = asignatura;
+    }
+
+    public void agregarEstudiante(Estudiante estudiante) {
+        estudiantes.add(estudiante);
+    }
+
+    public void asignarHorario(Horario horario) {
+        // lógica de asignación
+    }
+}
