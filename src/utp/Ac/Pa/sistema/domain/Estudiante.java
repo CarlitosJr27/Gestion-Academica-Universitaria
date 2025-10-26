3494fa57b7f03f79c0336de5a14d7755a1829adf
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:08:23 -0500

    Estudiante.java

--- src/utp/Ac/Pa/sistema/domain/Estudiante.java
+++ src/utp/Ac/Pa/sistema/domain/Estudiante.java
@@ -1,1 +1,36 @@
+package utp.Ac.Pa.sistema.domain;
+
+public class Estudiante {
+    private String id;
+    private String nombre;
+    private String correo;
+    private String carrera;
+    private String grupo;
+
+    public Estudiante(String id, String nombre, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public void asignarCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public void asignarGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public String obtenerPromedio() {
+        // Lógica para calcular promedio
+        return "Promedio calculado";
+    }
+
+    public void editar(String nuevoNombre, String nuevoCorreo) {
+        this.nombre = nuevoNombre;
+        this.correo = nuevoCorreo;
+    }
+
+    // Getters y setters omitidos por brevedad
+}
 
