f1824ce84de873c8c9185b644bb164af919db600
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:26:31 -0500

    Carrera.java

--- src/utp/Ac/Pa/sistema/domain/Carrera.java
+++ src/utp/Ac/Pa/sistema/domain/Carrera.java
@@ -1,1 +1,23 @@
+package utp.Ac.Pa.sistema.domain;
+
+import java.util.List;
+
+public class Carrera {
+    private String nombre;
+    private List<Requisito> requisitos;
+
+    public Carrera(String nombre, List<Requisito> requisitos) {
+        this.nombre = nombre;
+        this.requisitos = requisitos;
+    }
+
+    public boolean validarRequisitos(Estudiante estudiante) {
+        // lógica de validación
+        return true;
+    }
+
+    public void asignarEstudiante(Estudiante estudiante) {
+        estudiante.asignarCarrera(nombre);
+    }
+}
 
