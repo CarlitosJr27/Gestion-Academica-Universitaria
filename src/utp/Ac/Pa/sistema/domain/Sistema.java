b810429697828cfa00f258fce8d5d280eeaa33a0
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 18:30:30 -0500

    Sistema.java

--- src/utp/Ac/Pa/sistema/domain/Sistema.java
+++ src/utp/Ac/Pa/sistema/domain/Sistema.java
@@ -1,1 +1,22 @@
+package utp.Ac.Pa.sistema.domain;
+
+import java.util.List;
+
+public class Sistema {
+    private List<Usuario> usuarios;
+    private List<Estudiante> estudiantes;
+    private List<Docente> docentes;
+
+    public void registrarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    public void asignarRol(Usuario usuario, Rol rol) {
+        usuario = new Usuario(usuario.getUsername(), usuario.getPassword(), rol);
+    }
+
+    public boolean validarAcceso(Usuario usuario, String permiso) {
+        return usuario.getRol().validarAcceso(permiso);
+    }
+}
 
