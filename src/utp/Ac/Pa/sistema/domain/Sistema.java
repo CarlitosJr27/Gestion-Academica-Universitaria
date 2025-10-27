1b86c7e88ceca1d09f8c48a3af01d44f53cae3f5
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 19:32:26 -0500

    Sistema.java
    Added getters and setters for usuarios, estudiantes, and docentes.

--- src/utp/Ac/Pa/sistema/domain/Sistema.java
+++ src/utp/Ac/Pa/sistema/domain/Sistema.java
@@ -12,11 +12,35 @@ public class Sistema {
     }
 
     public void asignarRol(Usuario usuario, Rol rol) {
-        usuario = new Usuario(usuario.getUsername(), usuario.getPassword(), rol);
+        usuario.setRol(rol);
     }
 
     public boolean validarAcceso(Usuario usuario, String permiso) {
         return usuario.getRol().validarAcceso(permiso);
     }
-}
 
+    // Getters y Setters
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public List<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(List<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    public List<Docente> getDocentes() {
+        return docentes;
+    }
+
+    public void setDocentes(List<Docente> docentes) {
+        this.docentes = docentes;
+    }
+}
