4f5b385b53f0525f204aa9f3ba3720c95cfff97f
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 19:30:35 -0500

    Horario.java
    Added getters and setters for dia, horaInicio, and horaFin.

--- src/utp/Ac/Pa/sistema/domain/Horario.java
+++ src/utp/Ac/Pa/sistema/domain/Horario.java
@@ -15,5 +15,30 @@ public class Horario {
         return this.dia.equals(otro.dia) &&
                this.horaInicio.equals(otro.horaInicio);
     }
+
+    // Getters y Setters
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public String getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(String horaFin) {
+        this.horaFin = horaFin;
+    }
 }
 
