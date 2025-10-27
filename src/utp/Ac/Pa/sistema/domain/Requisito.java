7cc44d4d419fc86f6673416750b75234107d0c06
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-10-26 19:31:35 -0500

    Horario.java
    Added a new class 'Horario' with properties and methods for managing schedule conflicts.

--- src/utp/Ac/Pa/sistema/domain/Requisito.java
+++ src/utp/Ac/Pa/sistema/domain/Requisito.java
@@ -1,17 +1,43 @@
 package utp.Ac.Pa.sistema.domain;
 
-public class Requisito {
-    private String descripcion;
-    private Asignatura asignatura;
+public class Horario {
+    private String dia;
+    private String horaInicio;
+    private String horaFin;
 
-    public Requisito(String descripcion, Asignatura asignatura) {
-        this.descripcion = descripcion;
-        this.asignatura = asignatura;
+    public Horario(String dia, String horaInicio, String horaFin) {
+        this.dia = dia;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
     }
 
-    public boolean verificarCumplimiento(Estudiante estudiante) {
-        // lógica de verificación
-        return true;
+    public boolean validarConflicto(Horario otro) {
+        return this.dia.equals(otro.dia) &&
+               this.horaInicio.equals(otro.horaInicio);
+    }
+
+    // Getters y Setters
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public String getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(String horaFin) {
+        this.horaFin = horaFin;
     }
 }
-
