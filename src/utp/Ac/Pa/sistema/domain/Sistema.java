b5d94cf46fc677137f8e46e86c56906b57250f6a
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-11-23 18:28:23 -0500

    Sistema.java

--- src/utp/Ac/Pa/sistema/domain/Sistema.java
+++ src/utp/Ac/Pa/sistema/domain/Sistema.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 public class Sistema {
     private List<Usuario> usuarios;
-    private List<Estudiante> estudiantes;
-    private List<Docente> docentes;
 
     public void registrarUsuario(Usuario usuario) {
         usuarios.add(usuario);
@@ -15,32 +13,11 @@ public class Sistema {
         usuario.setRol(rol);
     }
 
-    public boolean validarAcceso(Usuario usuario, String permiso) {
-        return usuario.getRol().validarAcceso(permiso);
+    public boolean validarAcceso(Usuario usuario, String permiso, String password) {
+        return usuario.autenticar(password) && usuario.getRol().validarAcceso(permiso);
     }
 
     // Getters y Setters
-    public List<Usuario> getUsuarios() {
-        return usuarios;
-    }
-
-    public void setUsuarios(List<Usuario> usuarios) {
-        this.usuarios = usuarios;
-    }
-
-    public List<Estudiante> getEstudiantes() {
-        return estudiantes;
-    }
-
-    public void setEstudiantes(List<Estudiante> estudiantes) {
-        this.estudiantes = estudiantes;
-    }
-
-    public List<Docente> getDocentes() {
-        return docentes;
-    }
-
-    public void setDocentes(List<Docente> docentes) {
-        this.docentes = docentes;
-    }
+    public List<Usuario> getUsuarios() { return usuarios; }
+    public void setUsuarios(List<Usuario> usuarios) { this.usuarios = usuarios; }
 }
