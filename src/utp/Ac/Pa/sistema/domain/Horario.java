ca2fef0becd899337e968c5994dc22150e24bb35
CarlitosJr27 <carlosjenkins507@gmail.com>
2025-11-23 18:27:37 -0500

    Horario.java

--- src/utp/Ac/Pa/sistema/domain/Horario.java
+++ src/utp/Ac/Pa/sistema/domain/Horario.java
@@ -11,33 +11,13 @@ public class Horario {
         this.horaFin = horaFin;
     }
 
-    public boolean validarConflicto(Horario otro) {
+    public boolean validarConflicto(Horario otro, String grupoODocente) {
         return this.dia.equals(otro.dia) &&
                this.horaInicio.equals(otro.horaInicio);
     }
 
     // Getters y Setters
-    public String getDia() {
-        return dia;
-    }
-
-    public void setDia(String dia) {
-        this.dia = dia;
-    }
-
-    public String getHoraInicio() {
-        return horaInicio;
-    }
-
-    public void setHoraInicio(String horaInicio) {
-        this.horaInicio = horaInicio;
-    }
-
-    public String getHoraFin() {
-        return horaFin;
-    }
-
-    public void setHoraFin(String horaFin) {
-        this.horaFin = horaFin;
-    }
+    public String getDia() { return dia; }
+    public String getHoraInicio() { return horaInicio; }
+    public String getHoraFin() { return horaFin; }
 }
